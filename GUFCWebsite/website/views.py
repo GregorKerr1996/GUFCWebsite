f319eea027b4fd8f758f914559692ed6bb581c92
Conor <2199772m@student.gla.ac.uk>
2019-01-08 20:19:42 +0000

    Started templates and set up a couple views, ready to start on models

--- GUFCWebsite/website/views.py
+++ GUFCWebsite/website/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("GUFC <a href='/website/club/'>Club</a>")
+
+    context_dict = {'boldmessage': "Ballin never stops"}
+
+    return render(request, 'website/index.html', context = context_dict)
 
 def club(request):
-    return HttpResponse('The club page <a href="/website/">Index</a>')
+    
+    context_dict = {}
+    
+    return render(request, 'website/club.html', context = context_dict)
