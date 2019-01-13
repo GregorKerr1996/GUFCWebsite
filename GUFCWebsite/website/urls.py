f70a023deb25aeee8a6883b9302805cef24ebec3
Gregor Kerr <2139619k@student.gla.ac.uk>
2019-01-13 19:02:19 +0000

    Pages and Cool stuff

--- GUFCWebsite/website/urls.py
+++ GUFCWebsite/website/urls.py
@@ -3,5 +3,14 @@ from website import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^club/', views.club, name = 'club'),
+    url(r'^club/', views.club, name='club'),
+    url(r'^teamProfiles/', views.teamProfiles, name='teamProfiles'),
+    url(r'^firstTeam/', views.firstTeam, name='firstTeam'),
+    url(r'^secondTeam/', views.secondTeam, name='secondTeam'),
+    url(r'^thirdTeam/', views.thirdTeam, name='thirdTeam'),
+    url(r'^underTwentyOnes/', views.underTwentyOnes, name='underTwentyOnes'),
+    url(r'^matchReports/', views.matchReports, name='matchReports'),
+
+
+
 ]
--- GUFCWebsite/website/views.py
+++ GUFCWebsite/website/views.py
@@ -14,3 +14,36 @@ def club(request):
     context_dict = {}
     
     return render(request, 'website/club.html', context = context_dict)
+
+
+def teamProfiles(request):
+    context_dict = {}
+
+    return render(request, 'website/teamProfiles.html', context=context_dict)
+
+
+
+def firstTeam(request):
+    context_dict = {}
+
+    return render(request, 'website/firstTeam.html', context=context_dict)
+
+def secondTeam(request):
+    context_dict = {}
+
+    return render(request, 'website/secondTeam.html', context=context_dict)
+
+def thirdTeam(request):
+    context_dict = {}
+
+    return render(request, 'website/thirdTeam.html', context=context_dict)
+
+def underTwentyOnes(request):
+    context_dict = {}
+
+    return render(request, 'website/underTwentyOnes.html', context=context_dict)
+
+def matchReports(request):
+    context_dict = {}
+
+    return render(request, 'website/matchReports.html', context=context_dict)
