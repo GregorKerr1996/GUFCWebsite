025f2bd18b41099c1254d043ea799b839689ebcd
Gregor Kerr <gregorkerr@Gregors-MBP.broadband>
2019-09-07 18:55:38 +0100

    changes to settings.py

--- GUFCWebsite/settings.py
+++ GUFCWebsite/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'GUFCWebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
